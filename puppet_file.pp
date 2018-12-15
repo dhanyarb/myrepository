bdc59cb809e704fa760f0ca237edfc790ad21d18
dhanyarb <42954206+dhanyarb@users.noreply.github.com>
2018-12-15 07:40:28 +0000

    puppet_change

--- puppet_file.pp
+++ puppet_file.pp
@@ -5,4 +5,8 @@ file {'/root/file10':
       '/root/test10':
        ensure => 'file',
        content => 'this is test file';
+       
+      '/root/test100':
+       ensure => 'file',
+       content => 'this is test100 file';       
 }
